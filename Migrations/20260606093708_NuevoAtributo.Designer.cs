5116228b15d80f40b1ced5909ad40de2fe7ef5e6
vlaurido <vlaurido@espol.edu.ec>
2026-06-10 15:01:33 -0500

    Cambios para validaciones personalizadas, paginación y manejo de imágenes

--- Migrations/20260606093708_NuevoAtributo.Designer.cs
+++ Migrations/20260606093708_NuevoAtributo.Designer.cs
@@ -3,6 +3,7 @@ using LibreriaWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibreriaWeb.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260606093708_NuevoAtributo")]
+    partial class NuevoAtributo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260606093708_NuevoAtributo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibreriaWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class NuevoAtributo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagenRuta",
+                table: "Libros",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagenRuta",
+                table: "Libros");
+        }
+    }
+}
